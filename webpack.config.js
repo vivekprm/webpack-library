4636c7d9bbc9b4c20f5981c32ecb7b0ee17a043a
Vivek Mishra <v.mishra@f5.com>
2023-09-05 16:29:08 +0530

    npm library in webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "webpack-numbers.js",
+    globalObject: "this",
+    library: {
+      name: "webpackNumbers",
+      type: "umd",
+    },
+    clean: true,
+  },
+  externals: {
+    lodash: {
+      commonjs: "lodash",
+      commonjs2: "lodash",
+      amd: "lodash",
+      root: "_",
+    },
+  },
+};
